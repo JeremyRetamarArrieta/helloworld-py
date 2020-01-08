a417ea655ce49c3de5cbd3ae82672d21677674e5
Jeremy <jeremyretamararrieta@gmail.com>
2020-01-07 20:39:45 -0800

    hello world and name python

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+#This program says hello and asks for your name.
+print('Hello, world!')
+print('What is your name?') #asks for their name.
+myName = input()
+print('It is good to meet your,'+ '' + myName)
+print('The length of your name is:' + str(len(myName)))
+print('What is your age?') #asks for their age
+myAge = input()
+print('You will be' + ' ' + str(int(myAge)+ 1) +' ' + 'in a year.')
